663222ea1c55ec29631c476225701ef8b24b4eca
Alejandro Manzano <alejandromanzano.laforga@gmail.com>
2023-11-21 19:28:16 +0100

    constructores

--- src/poo/clases/Vehículo.java
+++ src/poo/clases/Vehículo.java
@@ -2,15 +2,28 @@ package poo.clases;
 
 public class Vehículo {
 
-    // 1. Atributos
-    String modelo;
-    String fabricante;
-    Double cv;
-    int year;
-    boolean sport;
+                // 1. Atributos
+                String modelo;
+                String fabricante;
+                double cv;
+                int year;
+                boolean sport;
 
-    // 2. Constructores
+                // 2. Constructores
+                public Vehículo(String fabricante, String modelo, double cv, int year, boolean sport){
+                        this.fabricante = fabricante;
+                        this.modelo = modelo;
+                        this.cv = cv;
+                        this.year = year;
+                        this.sport = sport;
+                }
 
-    // 3. Métodos
+                public Vehículo(String fabricante, String modelo){
+                        this.fabricante = fabricante;
+                        this.modelo = modelo;
+                }
+
+
+                // 3. Métodos
 
 }
