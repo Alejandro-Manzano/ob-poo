2792bd1947d0c34eb07df069f822a5c665f895be
Alejandro Manzano <alejandromanzano.laforga@gmail.com>
2023-11-21 19:08:42 +0100

    first object

--- src/poo/clases/Main.java
+++ src/poo/clases/Main.java
@@ -3,7 +3,7 @@ package poo.clases;
 public class Main {
 
     public static void main(String[] args) {
-        
+
     }
 
 }
---
+++ src/poo/clases/Vehículo.java
@@ -0,0 +1,16 @@
+package poo.clases;
+
+public class Vehículo {
+
+    // 1. Atributos
+    String modelo;
+    String fabricante;
+    Double cv;
+    int year;
+    boolean sport;
+
+    // 2. Constructores
+
+    // 3. Métodos
+
+}
